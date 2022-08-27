4f6f205c2530795af7ceea38241cf45cd9b5e737
Tanner Leroy <99232416+TannerLeroyCodes@users.noreply.github.com>
2022-08-27 12:35:39 -0400

    Time: 164 ms (27.75%), Space: 46.8 MB (49.88%) - LeetHub

---
+++ 283-move-zeroes/283-move-zeroes.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function(nums) {
+    
+    let index = 0;
+    
+    for(let i=0; i < nums.length ; i++){
+      if (nums[i] !== 0){
+          nums[index] = nums[i];
+          index++
+          
+      }
+    }
+    
+    for (let i = index; i < nums.length; i++){
+        nums[i] = 0;
+    }
+    
+};
