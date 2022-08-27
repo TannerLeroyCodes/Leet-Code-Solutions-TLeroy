a6f033d26cef0191293bcbe488e29d7548ed8ab4
Tanner Leroy <99232416+TannerLeroyCodes@users.noreply.github.com>
2022-08-27 12:56:10 -0400

    Time: 106 ms (23.58%), Space: 41.8 MB (91.33%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.js
+++ 88-merge-sorted-array/88-merge-sorted-array.js
@@ -9,7 +9,7 @@ var merge = function(nums1, m, nums2, n) {
     let first = m-1;
     let second =n-1;
     
-    let i = m + n -1;
+    let i = nums1.length-1;
     
     while(second >= 0){
         let fVal = nums1[first];
