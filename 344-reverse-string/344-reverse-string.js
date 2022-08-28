048c7355f04a5422695bc946d2d8c70eba260cb3
Tanner Leroy <99232416+TannerLeroyCodes@users.noreply.github.com>
2022-08-28 14:08:35 -0400

    Time: 150 ms (38.43%), Space: 48.8 MB (90.82%) - LeetHub

---
+++ 344-reverse-string/344-reverse-string.js
@@ -0,0 +1,11 @@
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+var reverseString = function(s) {
+    let low = 0;
+    let high = s.length -1 ;
+    while (low < high){
+        [s[low++] ,s[high--]] = [s[high],s[low]];
+    }
+};
