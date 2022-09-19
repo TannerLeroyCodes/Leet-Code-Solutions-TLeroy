98f4661f830f9fc05c6e23d63e88a7514dcacf83
Tanner Leroy <99232416+TannerLeroyCodes@users.noreply.github.com>
2022-09-19 13:50:40 -0400

    Time: 385 ms (12.98%), Space: 50.9 MB (70.50%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.js
+++ 9-palindrome-number/9-palindrome-number.js
@@ -3,11 +3,29 @@
  * @return {boolean}
  */
 var isPalindrome = function(x) {
-    let reversed = x.toString().split('').reverse().join('')
     
-    if (reversed == x){
-        return true 
-    } else {
-        return false 
+    
+    let reversedStr = ''
+    let xStr = x.toString()
+    
+    for(let i of xStr){
+        reversedStr = i + reversedStr
     }
+     if (reversedStr == xStr){
+         return true
+     } else {
+         return false 
+     }
+    
+    
+    
+    
+    
+//     let reversed = x.toString().split('').reverse().join('')
+    
+//     if (reversed == x){
+//         return true 
+//     } else {
+//         return false 
+    // }
 };
